8bb9ed265acd4ef59f26874f3438af4c0b1bac86
Martinez Lara Jose Eduardo <martinezlara_joseeduardo@hotmail.com>
2021-10-19 22:24:49 -0500

    configuration files finished

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const CleanPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: process.env.NODE_ENV || 'production',
+    target: "web",
+
+    entry: './src/index.tsx',
+    module: {
+        rules: [
+            {
+                test: /\.(ts|js)x?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.(png|jpe?g|gif|ico)$/i,
+                loader: 'file-loader',
+            },
+            {
+                test: /\.(sa|sc|c)ss$/i,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            // Prefer `dart-sass`
+                            implementation: require("sass"),
+                        },
+                    }
+                ],
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"],
+    },
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js",
+        publicPath: "/"
+    },
+    devServer: {
+        port: 8081,
+        host: "local-ipv4",
+        historyApiFallback: true,
+    },
+    plugins: [
+        new CleanPlugin.CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html',
+        }),
+    ],
+};
