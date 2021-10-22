73325329229be4c84f1325481a745e9685b926ff
Martinez Lara Jose Eduardo <martinezlara_joseeduardo@hotmail.com>
2021-10-22 02:21:43 -0500

    Button component completed

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -8,12 +8,13 @@ interface ButtonProps {
     onSubmit: () => void;
 }
 
-const Button: FC<ButtonProps> = ({type = "default", children}) => {
-    return <span className={clsx("button", "bordered", {
-        "button-default": type === "default",
-        "button-danger": type === "danger",
-        "button-primary": type === "primary",
-    })}>
+const Button: FC<ButtonProps> = ({type = "default", onSubmit, children}) => {
+    return <span onClick={onSubmit}
+                 className={clsx("button", "bordered", {
+                     "button-default": type === "default",
+                     "button-danger": type === "danger",
+                     "button-primary": type === "primary",
+                 })}>
         {children}
     </span>
 }
