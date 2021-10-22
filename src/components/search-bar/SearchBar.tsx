8a9b5743f2c0c012b0c606901f42d6052054de84
Martinez Lara Jose Eduardo <martinezlara_joseeduardo@hotmail.com>
2021-10-22 02:22:14 -0500

    SearchBar keydown press event added

--- src/components/search-bar/SearchBar.tsx
+++ src/components/search-bar/SearchBar.tsx
@@ -1,14 +1,33 @@
-import React, {FC} from "react"
+import React, {FC, useState} from "react"
 import searchLogo from "../../static/images/search.svg"
 import "./styles.sass"
 
-const SearchBar: FC = () => {
+interface SearchBarProps {
+    onSubmit: (input: string) => void
+}
+
+const SearchBar: FC<SearchBarProps> = ({onSubmit}) => {
+
+    const [input, setInput] = useState('')
+
     return <div className="searchbar-container">
-        <div className="flex" style={{justifyContent: "center", position: "relative"}}>
-            <input type="text" className="searchbar-input light-shadow"
-                   placeholder=" Search"/>
-            <div className="searchbar-logo">
-                <img src={searchLogo} alt="not found" width="100%" height="100%"/>
+        <div className="flex"
+             style={{justifyContent: "center", position: "relative"}}>
+            <input type="text"
+                   className="searchbar-input light-shadow"
+                   placeholder=" Search"
+                   value={input}
+                   onChange={e => setInput(e.target.value as string)}
+                   onKeyDown={e => {
+                       if (e.keyCode === 13)
+                           onSubmit(input)
+                   }}/>
+            <div className="searchbar-logo"
+                 onClick={() => onSubmit(input)}>
+                <img src={searchLogo}
+                     alt="not found"
+                     width="100%"
+                     height="100%"/>
             </div>
         </div>
     </div>
